4f60d2a2b2bb160a74adc52f7cb23632d0d63b1f
busyfree <geek.snail@qq.com>
2021-04-26 17:40:22 +0800

    clean

--- server/webgin/controllers/api/api.go
+++ server/webgin/controllers/api/api.go
@@ -6,7 +6,6 @@ import (
 	"net/url"
 
 	"github.com/busyfree/shorturl-go/service"
-	"github.com/busyfree/shorturl-go/util/log"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -19,7 +18,6 @@ type QueryForm struct {
 
 func (c *LinkController) Redirect(ctx *gin.Context) {
 	var form QueryForm
-	logger := log.Get(ctx)
 	code := ctx.Param("code")
 	if len(code) == 0 {
 		ctx.String(http.StatusOK, "welcome to short url service")
@@ -37,26 +35,21 @@ func (c *LinkController) Redirect(ctx *gin.Context) {
 	err := linkService.FindOneLinkByKey(ctx)
 	if err != nil {
 		if err == sql.ErrNoRows || err == mongo.ErrNoDocuments {
-			//ctx.String(http.StatusOK, "welcome to short url service")
-			ctx.JSON(http.StatusOK, gin.H{"status": 0, "message": err.Error()})
+			ctx.String(http.StatusOK, "welcome to short url service")
 			return
 		}
 		ctx.JSON(http.StatusOK, gin.H{"status": 0, "message": err.Error()})
 		return
 	}
-	logger.Infof("linkService.Dao.Url:%+v", linkService.Dao)
 	if len(linkService.Dao.Url) == 0 {
 		ctx.String(http.StatusOK, "welcome to short url service")
 		return
 	}
-
-	logger.Infof("linkService.Dao.Url:%s", linkService.Dao.Url)
 	target, errUnescape := url.QueryUnescape(linkService.Dao.Url)
 	if errUnescape != nil {
 		ctx.String(http.StatusOK, errUnescape.Error())
 		return
 	}
-	logger.Infof("target:%s", target)
 	//@todo write some analysis data to nsq or other async queue
 	ctx.Redirect(http.StatusFound, target)
 	return
